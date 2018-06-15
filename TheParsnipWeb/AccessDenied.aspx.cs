40bd0b5500d9134ecb567a3d67b3b80cac28d1a6
vamon1122 <35330025+vamon1122@users.noreply.github.com>
2018-06-15 21:12:56 +0100

    Uac Bug Fixes

---
+++ TheParsnipWeb/AccessDenied.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UacApi;
+
+namespace TheParsnipWeb
+{
+    public partial class AccessDenied : System.Web.UI.Page
+    {
+        Account MyAccount;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MyAccount = new Account();
+
+            
+            if (Request.QueryString["url"] != null)
+            {
+                if (!MyAccount.LogIn())
+                {
+                    Info.Text = String.Format("You aren't allowed to visit \"{0}\". Nice try {1}...", Request.QueryString["url"], MyAccount.Fname);
+                }
+                else
+                {
+                    Response.Redirect("LogInBarrier.aspx?" + Request.QueryString["url"]);
+                }
+
+                
+            }
+            else
+            {
+                Info.Text = "Why are you trying to get back here? :P";
+            }
+
+            
+        }
+    }
+}
---
+++ TheParsnipWeb/Admin.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UacApi;
+
+namespace TheParsnipWeb
+{
+    public partial class Admin : System.Web.UI.Page
+    {
+        Account MyAccount;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MyAccount = new Account();
+            if (!MyAccount.LogIn())
+            {
+                Response.Redirect("LogInBarrier.aspx?url=Admin.aspx");
+            }
+
+            if(MyAccount.AccountType != "admin")
+            {
+                Response.Redirect("AccessDenied.aspx?url=Admin.aspx");
+            }
+        }
+    }
+}
