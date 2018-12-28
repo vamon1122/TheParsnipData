830420312aa928ae52693e97160393121e1360d3
vamon1122 <35330025+vamon1122@users.noreply.github.com>
2018-12-28 12:06:23 +0000

    Templates amended

--- ParsnipWebsite/_NEW TEMPLATE.aspx.cs
+++ ParsnipWebsite/_NEW TEMPLATE.aspx.cs
@@ -15,6 +15,10 @@ namespace TheParsnipWeb
         User myUser;
         protected void Page_Load(object sender, EventArgs e)
         {
+            //We secure the page using the UacApi. 
+            //This ensures that the user is logged in etc
+            //You only need to change where it says '_NEW TEMPLATE'.
+            //Change this to match your page name without the '.aspx' extension.
             myUser = Uac.SecurePage("_NEW TEMPLATE", this, Data.deviceType);
         }
     }
--- ParsnipWebsite/_TEMPLATE.aspx.cs
+++ ParsnipWebsite/_TEMPLATE.aspx.cs
@@ -1,27 +1,25 @@
 ﻿using System;
-using UacApi;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using UacApi;
+using LogApi;
+using CookieApi;
 
 namespace TheParsnipWeb
 {
     public partial class _TEMPLATE : System.Web.UI.Page
     {
-        private User MyAccount;
+        private User myUser;
         protected void Page_Load(object sender, EventArgs e)
         {
-            MyAccount = new User();
-            if (!MyAccount.LogIn())
-            {
-                Response.Redirect("login.aspx?url=photos.aspx");
-            }
-            else
-            {
-
-            }
+            //We secure the page using the UacApi. 
+            //This ensures that the user is logged in etc
+            //You only need to change where it says '_NEW TEMPLATE'.
+            //Change this to match your page name without the '.aspx' extension.
+            myUser = Uac.SecurePage("_NEW TEMPLATE", this, Data.deviceType);
         }
     }
 }
