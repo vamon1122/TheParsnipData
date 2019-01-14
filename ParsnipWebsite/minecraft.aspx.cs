bc96f649f7aa833bbf6d2a1410e02cca64bb1f6e
vamon1122 <35330025+vamon1122@users.noreply.github.com>
2019-01-14 15:52:04 +0000

    Removed some unnecessary logging

--- ParsnipWebsite/minecraft.aspx.cs
+++ ParsnipWebsite/minecraft.aspx.cs
@@ -16,12 +16,12 @@ namespace TheParsnipWeb
         {
             if(string.IsNullOrEmpty(Data.deviceType) || string.IsNullOrWhiteSpace(Data.deviceType))
             {
-                new LogEntry(Guid.Empty) { text = "Device type was null. Redirecting to getDeviceInfo" };
+                //new LogEntry(Guid.Empty) { text = "Device type was null. Redirecting to getDeviceInfo" };
                 Response.Redirect("getDeviceInfo?url=minecraft");
             }
             else
             {
-                new LogEntry(Guid.Empty) { text = string.Format("Device type ({0}) was not null. Not redirecting to getDeviceInfo", Data.deviceType )};
+                //new LogEntry(Guid.Empty) { text = string.Format("Device type ({0}) was not null. Not redirecting to getDeviceInfo", Data.deviceType )};
             }
             
             myUser = Uac.SecurePage("minecraft", this, Data.deviceType);
