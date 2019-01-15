11e7febb82dc105d589692601daa097e463a24a6
vamon1122 <35330025+vamon1122@users.noreply.github.com>
2019-01-15 10:48:09 +0000

    Logout improvements

--- ParsnipWebsite/logout.aspx.cs
+++ ParsnipWebsite/logout.aspx.cs
@@ -13,6 +13,9 @@ namespace TheParsnipWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (string.IsNullOrEmpty(Data.deviceType) || string.IsNullOrWhiteSpace(Data.deviceType))
+                Response.Redirect("getdeviceinfo?url=logout");
+
             User myUser = new User("logout get name");
             myUser.LogIn();
             new LogEntry(myUser.Id) { text = String.Format("{0} logged out from {1} '{2}' device", myUser.FullName, myUser.PosessivePronoun, Data.deviceType) };
