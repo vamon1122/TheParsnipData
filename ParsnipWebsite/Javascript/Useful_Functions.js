1928a195987cd583ddd217bc01cd7725ac4f0b38
vamon1122 <35330025+vamon1122@users.noreply.github.com>
2019-02-25 23:56:13 +0000

    Some formatting on different screen sizes

--- ParsnipWebsite/Javascript/Useful_Functions.js
+++ ParsnipWebsite/Javascript/Useful_Functions.js
@@ -561,7 +561,7 @@ function second() {
 
 
 //Mobile / Desktop CSS ----- KEYWORDS: Style, sheet,  stylesheet
-/*
+
 {
     if (elementExists("link_style")) {
         if (navigator.userAgent.match(/Android/i)
@@ -581,27 +581,36 @@ function second() {
         }
     }
 }
-*/
 
-DetectOrientation();
 
-function DetectOrientation() {
-    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+ApplyStyle();
+
+function ApplyStyle() {
+
     var link_style = document.getElementById("link_style");
 
+    if (isMobile())
+    {    
+        var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-    if (h > w) {
-
-        link_style.setAttribute("href", "../Css/Mobile_Style.css");
+        if (h > w)
+        {
+            link_style.setAttribute("href", "../Css/Mobile_Portrait_Style.css");
+        }
+        else
+        {
+            link_style.setAttribute("href", "../Css/Mobile_Landscape_Style.css");
+        }
     }
-    else {
+    else
+    {   
         link_style.setAttribute("href", "../Css/Desktop_Style.css");
     }
 }
 
 window.addEventListener("orientationchange", function () {
-    DetectOrientation();
+    ApplyStyle();
 });
 
 
