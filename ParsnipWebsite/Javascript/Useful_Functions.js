1e368979db34970cf0e46bd761b5a7ef1092f4be
vamon1122 <35330025+vamon1122@users.noreply.github.com>
2019-02-25 23:31:06 +0000

    Css now based on orientation (rather than mobile / desktop)

--- ParsnipWebsite/Javascript/Useful_Functions.js
+++ ParsnipWebsite/Javascript/Useful_Functions.js
@@ -561,6 +561,7 @@ function second() {
 
 
 //Mobile / Desktop CSS ----- KEYWORDS: Style, sheet,  stylesheet
+/*
 {
     if (elementExists("link_style")) {
         if (navigator.userAgent.match(/Android/i)
@@ -580,6 +581,29 @@ function second() {
         }
     }
 }
+*/
+
+DetectOrientation();
+
+function DetectOrientation() {
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    var link_style = document.getElementById("link_style");
+
+
+    if (h > w) {
+
+        link_style.setAttribute("href", "../Css/Mobile_Style.css");
+    }
+    else {
+        link_style.setAttribute("href", "../Css/Desktop_Style.css");
+    }
+}
+
+window.addEventListener("orientationchange", function () {
+    DetectOrientation();
+});
+
 
 
 //Mobile / Desktop Check ----- KEYWORDS: Device, phone, pc
