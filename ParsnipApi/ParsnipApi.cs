51c09e43e8333db197836d49c2bcc2e039925677
vamon1122 <35330025+vamon1122@users.noreply.github.com>
2019-03-19 19:50:39 +0000

    Fixed api connection string

--- ParsnipApi/ParsnipApi.cs
+++ ParsnipApi/ParsnipApi.cs
@@ -13,7 +13,7 @@ namespace ParsnipApi
 {
     public static class Parsnip
     {
-        public static readonly string apiUrl = @"api/";
+        public static readonly string apiUrl = @"/api/";
         public static readonly HttpClient client = new HttpClient();
         public static readonly LogWriter AsyncLog = new LogWriter("Async_Login.txt", HttpContext.Current.Server.MapPath("~"));
 
@@ -29,17 +29,17 @@ namespace ParsnipApi
         ////public static string sqlConnectionString = ";";
 
         //Live
-        //public static string sqlConnectionString = "Server=198.38.83.33;Database=vamon112_parsnipdb;Uid=vamon112_ben;Password=ccjO07JT";
-        //public static readonly string baseAddress = "https://www.theparsnip.co.uk/";
+        public static readonly string sqlConnectionString = "Server=198.38.83.33;Database=vamon112_parsnipdb;Uid=vamon112_ben;Password=ccjO07JT";
+        public static readonly string baseAddress = "https://www.theparsnip.co.uk";
 
         //Home
         //public static readonly string sqlConnectionString = @"Data Source=BEN-PC\SQLEXPRESS;Initial Catalog=ParsnipTestDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-        //public static readonly string baseAddress = "http://localhost:59622/";
+        //public static readonly string baseAddress = "http://localhost:59622";
 
         //Branson
-        public static readonly string sqlConnectionString = @"Data Source=branson\sqlexpress;Initial Catalog=ParsnipTestDb;Integrated Security=True";
-        public static readonly string baseAddress = "http://localhost:59622/";
-        //public static readonly string baseAddress = "http://localhost:8080/";
+        //public static readonly string sqlConnectionString = @"Data Source=branson\sqlexpress;Initial Catalog=ParsnipTestDb;Integrated Security=True";
+        //public static readonly string baseAddress = "http://localhost:59622";
+        //public static readonly string baseAddress = "http://localhost:8080";
 
         public static SqlConnection GetOpenDbConnection()
         {
