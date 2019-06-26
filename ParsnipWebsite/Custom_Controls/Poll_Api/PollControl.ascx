59b963ecbcf45f038a217e36d9063c90064e9111
vamon1122 <35330025+vamon1122@users.noreply.github.com>
2019-06-26 14:42:43 +0100

    Removed parsnip website from data code

--- ParsnipWebsite/Custom_Controls/Poll_Api/PollControl.ascx
+++
@@ -1,3 +0,0 @@
-﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="PollControl.ascx.cs" Inherits="ParsnipWebsite.CustomControls.PollApi.PollControl" %>
-<form runat="server">
-    </form>
--- ParsnipWebsite/Javascript/Focus_Image.js
+++
@@ -1,9 +0,0 @@
-﻿try {
-    var url_string = window.location.href
-    var url = new URL(url_string);
-    var id = document.querySelector('[id*="' + url.searchParams.get("imageid") + '"]').id;
-    document.getElementById(id).scrollIntoView();
-}
-catch (e) {
-    //This does not work on older browsers.
-}
