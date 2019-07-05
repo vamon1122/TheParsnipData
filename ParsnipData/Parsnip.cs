77ca1966e937509b4d97c58cd8b6f023c96125f3
vamon1122 <35330025+vamon1122@users.noreply.github.com>
2019-07-05 14:41:22 +0100

    Added config file

--- ParsnipData/Parsnip.cs
+++ ParsnipData/Parsnip.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace ParsnipData
 {
     public static class Parsnip
     {
-        internal static readonly string sqlConnectionString = "Server=198.38.83.33;Database=vamon112_parsnipdb;Uid=vamon112_ben;Password=ccjO07JT";
+        internal static readonly string sqlConnectionString = ConfigurationManager.ConnectionStrings["ParsnipDb"].ConnectionString;
+        //internal static readonly string sqlConnectionString = "Server=198.38.83.33;Database=vamon112_parsnipdb;Uid=vamon112_ben;Password=ccjO07JT";
         //internal static readonly string sqlConnectionString = @"Data Source=BEN-PC\SQLEXPRESS;Initial Catalog=ParsnipTestDb;Integrated Security=True";
         public static SqlConnection GetOpenDbConnection()
         {
